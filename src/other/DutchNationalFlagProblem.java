bca3db2586b76b54864e52dc4e05ebe0da4bd5eb
szhou1 <zhousteve3@gmail.com>
2017-01-04 23:25:02 +0700

    Start Dutch National Flag problem

---
+++ src/other/DutchNationalFlagProblem.java
@@ -0,0 +1,30 @@
+package other;
+
+/**
+ * Given an array A[] consisting 0s, 1s and 2s, write a function that sorts A[]. The functions should put all 0s first, then all 1s and all 2s in last.
+ The trick is to do this with n moves and n checks
+Example
+Input = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
+Output = {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2}
+
+ * @author szhou
+ *
+ */
+public class DutchNationalFlagProblem {
+    
+    private int [] arr;
+    
+    public DutchNationalFlagProblem(int [] arr) {
+        this.arr = arr;
+    }
+    
+    public int [] sort() {
+        int top = 0;
+        int mid = 0;
+        int bot = arr.length;
+        
+        
+        
+        return arr;
+    }
+}
